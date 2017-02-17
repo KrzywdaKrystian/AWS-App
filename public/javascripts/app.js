0cd6a2eb5ea73d519d20f8e9fdd23f1a162f93a5
Krystian Krzywda <krystiankrzywda22@gmail.com>
2017-02-17 01:23:26 +0100

    remove photo from gallery

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -375,28 +375,22 @@ if(e)var t=e.constructor,r=e.length;return"function"==typeof t&&t.prototype===e|
 
 function GalleryController($scope) {
 
-    // http://stackoverflow.com/questions/32702431/display-images-fetched-from-s3
-    $scope.creds = CONFIG_CREDS;
+    var creds = CONFIG_CREDS;
+    var bucket = new AWS.S3();
+    var params = {
+        Bucket: creds.bucket
+    };
+
     $scope.gallery = [];
     $scope.inProgress = false;
 
-    // listObjects
-    function encode(data)
-    {
-        var str = data.reduce(function(a,b){ return a+String.fromCharCode(b) },'');
-        return btoa(str).replace(/.{76}(?=.)/g,'$&\n');
-    }
+    $scope.deletePhoto = deletePhoto;
 
+    // listObjects
     (function () {
         $scope.inProgress = true;
 
-        var bucket = new AWS.S3();
-
-        var params = {
-            Bucket: $scope.creds.bucket
-        };
-
-        bucket.listObjects(params, function(err, data) {
+        bucket.listObjects(params, function (err, data) {
             // an error occurred
             if (err) {
                 alert(err + ' ' + err.stack);
@@ -406,22 +400,25 @@ function GalleryController($scope) {
                 setTimeout(function () {
                     $scope.$apply(function () {
                         console.log(data.Contents);
-                        for(var i = 0; i < data.Contents.length; i++) {
+                        for (var i = 0; i < data.Contents.length; i++) {
 
-                            if(data.Contents[i]['Size'] > 0) {
+                            if (data.Contents[i]['Size'] > 0) {
+                                var key = data.Contents[i]['Key'];
                                 bucket.getObject({
-                                    Bucket: $scope.creds.bucket,
-                                    Key: data.Contents[i]['Key']
-                                },function(err,file) {
-                                    if(err) {
-                                        console.error(err);
-                                    }
-                                    else {
+                                    Bucket: creds.bucket,
+                                    Key: key
+                                }, function (err, file) {
+                                    if (!err) {
                                         setTimeout(function () {
                                             $scope.$apply(function () {
-                                                $scope.gallery.push('data:image/jpeg;base64,' + encode(file.Body));
+                                                $scope.gallery.push({
+                                                    'key': key,
+                                                    'src': 'data:image/jpeg;base64,' + encode(file.Body)
+                                                });
                                             });
                                         });
+                                    } else {
+                                        console.error(err);
                                     }
                                 });
                             }
@@ -432,7 +429,38 @@ function GalleryController($scope) {
             }
         });
 
-    }())
+    }());
+
+    function encode(data) {
+        var str = data.reduce(function (a, b) {
+            return a + String.fromCharCode(b)
+        }, '');
+        return btoa(str).replace(/.{76}(?=.)/g, '$&\n');
+    }
+
+    function deletePhoto(index) {
+        console.log(index);
+
+        var photo = $scope.gallery[index];
+
+        var object = {
+            Bucket: creds.bucket,
+            Key : photo.key
+        };
+
+        bucket.deleteObject(object, function (err, data) {
+            if (err) {
+                alert(err);
+            }
+            else {
+                setTimeout(function () {
+                    $scope.$apply(function () {
+                        $scope.gallery.splice(index, 1);
+                    });
+                });
+            }
+        });
+    }
 
 }
 function MainController($scope) {
